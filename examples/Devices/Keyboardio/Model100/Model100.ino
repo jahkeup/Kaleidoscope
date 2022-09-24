ad986c6fc9f0b9df1ce8d2a4afd85d722bae1858
Jacob Vallejo <jake@jahkeup.com>
2022-09-24 16:02:26 -0700

    fix: i type too quickly to use this

--- examples/Devices/Keyboardio/Model100/Model100.ino
+++ examples/Devices/Keyboardio/Model100/Model100.ino
@@ -196,8 +196,8 @@ KEYMAPS(
   [PRIMARY] = KEYMAP_STACKED
   (___,          Key_1, Key_2, Key_3, Key_4, Key_5, Key_LEDEffectNext,
    Key_Backtick, Key_Q, Key_W, Key_E, Key_R, Key_T, Key_Tab,
-   Key_PageUp,   Key_A, Key_S, Key_D, CTL_T(F), Key_G,
-   Key_PageDown, Key_Z, Key_X, Key_C, Key_V, Key_B, GUI_T(Escape),
+   Key_PageUp,   Key_A, Key_S, Key_D, Key_F, Key_G,
+   Key_PageDown, Key_Z, Key_X, Key_C, Key_V, Key_B, Key_Escape,
    Key_LeftControl, Key_Backspace, Key_LeftGui, Key_LeftShift,
    ShiftToLayer(FUNCTION),
 
@@ -267,8 +267,8 @@ KEYMAPS(
   [COLEMAK] = KEYMAP_STACKED
   (___,          Key_1, Key_2, Key_3, Key_4, Key_5, Key_LEDEffectNext,
    Key_Backtick, Key_Q, Key_W, Key_F, Key_P, Key_G, Key_Tab,
-   Key_PageUp,   Key_A, Key_R, Key_S, CTL_T(T), Key_D,
-   Key_PageDown, Key_Z, Key_X, Key_C, Key_V, Key_B, GUI_T(Escape),
+   Key_PageUp,   Key_A, Key_R, Key_S, Key_T, Key_D,
+   Key_PageDown, Key_Z, Key_X, Key_C, Key_V, Key_B, Key_Escape,
    Key_LeftControl, Key_Backspace, Key_LeftGui, Key_LeftShift,
    ShiftToLayer(FUNCTION),
 
